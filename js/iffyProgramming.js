04a8baa9c79ee31bb68c65cee68bc06c0a08deb2
almostNathan <nathanallenall@gmail.com>
2021-09-28 14:41:55 -0400

    started iffyProgramming

---
+++ js/iffyProgramming.js
@@ -0,0 +1,30 @@
+/*
+    filename: iffyProgramming.html
+    author: Nathan Allen
+    created: 9/28/21
+    purpose: demonstrate the use of conditional statements
+    mod history:
+
+*/
+
+//when page loads
+$(document).ready(function(){
+    //ask the user if they will arrive on time
+    var strUserInput = prompt('Will you arrive on time?').toUpperCase
+    //get the element used for output
+    var divOutput = document.getElementById("")
+
+    //if user said yes
+    if (strUserInput == "YES"){
+        
+    }
+        //respond positively
+    //if user said no
+    if (strUserInput == "NO"){
+
+    }
+        //respond negatively
+
+
+
+})
