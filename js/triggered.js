bf5471614eb1a70b01a3f68fc9c49ccbd8704991
almostNathan <nathanallenall@gmail.com>
2021-11-10 18:48:28 -0500

    started triggered

---
+++ js/triggered.js
@@ -0,0 +1,16 @@
+/*
+    filename: triggered.js
+    author: Nathan Allen
+    created: 11/10/21
+    purpose: demonstrate the use of triggers
+    mod history:
+
+*/
+
+$(document).ready(function(){
+    elBody = $("body")
+    for (var i = 0; i<5;i++){
+        elBody.append("<div></div>")
+        
+    }
+})
